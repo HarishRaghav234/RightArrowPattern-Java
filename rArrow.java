f7b20ffe3f7751f2651cd4bca2e4bc39eca2628c
Harish Raghav <86465455+Zeus404-XD@users.noreply.github.com>
2024-03-05 20:57:02 +0530

    Update rArrow.java

--- rArrow.java
+++ rArrow.java
@@ -1,27 +1,22 @@
 public class rArrow {
     public static void main(String[] args){
-        int StarLoop=1,StarLoopTwo=2;
+        int StarLoopOne=1,StarLoopTwo=2;
         int StarinMiddle=8;
-        /*
-         *      *
-         *      **
-         * ********
-         * *********
-         * ********
-         *      **
-         *      *
-         */
         int spaces=5;
+
+        //For the first two lines
         for(int i=0;i<2;i++){
             for(int j=0;j<spaces;j++){
                 System.out.print(" ");
             }
-            for(int k=0;k<StarLoop;k++){
+            for(int k=0;k<StarLoopOne;k++){
                 System.out.print("*");
             }
-            StarLoop++;
+            StarLoopOne++;
             System.out.println();
         }
+
+        //for the next three lines without spaces
         for(int l=0;l<3;l++){
             for(int m=0;m<StarinMiddle;m++){
                 System.out.print("*");
@@ -34,6 +29,8 @@ public class rArrow {
                 StarinMiddle++;
             }
         }
+
+        //for the last two lines
         for(int n=0;n<2;n++){
             for(int o=0;o<spaces;o++){
                System.out.print(" ");
